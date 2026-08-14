47c5e5ce02298c395b9fddb9a260f38aff38dd0c
Ali Haidar <ecommerceedittor@gmail.com>
2026-08-14 07:02:40 +0500

    Add CEREBRAS_API_KEY to configuration

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ import os
 class Config:
     # --- LLM / Scripting APIs ---
     GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
+    CEREBRAS_API_KEY = os.environ.get("CEREBRAS_API_KEY", "")
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
 
     # --- Research APIs ---
