46530ce57fc2853eb71dac80fd059d4b0982e7d5
Ali Haidar <ecommerceedittor@gmail.com>
2026-08-22 19:35:27 +0500

    Delete orchestrator_shorts.py

--- orchestrator_shorts.py
+++
@@ -1,8 +0,0 @@
-"""
-orchestrator_shorts.py
-Entry point for the daily Shorts workflow. Triggered 2x/day.
-"""
-from core.pipeline_runner import run_pipeline
-
-if __name__ == "__main__":
-    run_pipeline(video_type="shorts")
